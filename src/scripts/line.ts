5f19365954bb0d161945265311a4e24a2c545b04
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-02-28 22:18:32 -0300

    saving state of the bg fillrect

--- src/scripts/line.ts
+++ src/scripts/line.ts
@@ -11,17 +11,19 @@ export function spawn({ id, x, y, type, initialX, initialY, arise, die }: Omit<L
     start: Math.random() < 0.1,
     arise,
     die,
+    died: false,
   };
 
   return line;
 }
 
 export function die(line: Line) {
-  if (!line.died)
-    if (line.type === "row") {
-      if (line.x > line.die) line.died = true;
-    } else if (line.type === "col")
-      if (line.y > line.die) {
-        line.died = true;
-      }
+  //   if (!line.died)
+  if (line.type === "row") {
+    if (line.x > line.die) line.died = true;
+  } else if (line.type === "col") {
+    if (line.y > line.die) {
+      line.died = true;
+    }
+  }
 }
--- src/scripts/loop.ts
+++ src/scripts/loop.ts
@@ -5,6 +5,7 @@ let canvas = document.querySelector("canvas");
 let ctx = canvas?.getContext("2d");
 
 let lines: Line[] = [];
+let test: number[] = [];
 function drawCanvas() {
   if (!canvas) return;
 
@@ -21,34 +22,40 @@ function drawCanvas() {
 
     //horizontal position
     for (var x = 0; x <= canvas.width; x += padding) {
+      if (Math.random() < 0.1) {
+        lines.push(
+          spawn({
+            id: crypto.randomUUID(),
+            x,
+            y: 0,
+            type: "col",
+            initialX: x,
+            initialY: 0,
+            arise: x,
+            die: canvas.height,
+          })
+        );
+      }
+    }
+
+    // //vertical position
+    for (var y = 60; y <= canvas.height - 60 * 2; y += padding) {
+      // if (Math.random() < 0.1) {
       lines.push(
         spawn({
           id: crypto.randomUUID(),
-          x,
-          y: 0,
-          type: "col",
-          initialX: x,
-          initialY: 0,
-          arise: x,
-          die: canvas.height,
+          x: 0,
+          y,
+          type: "row",
+          initialX: 0,
+          initialY: y,
+          arise: y,
+          die: canvas.width,
         })
       );
+      // }
     }
-
-    //vertical position
-    for (var y = 0; y <= canvas.height; y += padding) {
-      lines.push({
-        id: crypto.randomUUID(),
-        x: 0,
-        y,
-        type: "row",
-        initialX: 0,
-        initialY: y,
-        start: Math.random() < 0.1,
-        arise: y,
-        die: canvas.width,
-      });
-    }
+    console.log("total:", lines.length);
   }
 }
 drawCanvas();
@@ -57,48 +64,52 @@ let speed = Math.random() * 20;
 let counter = 0;
 counter += increase;
 let start = 300;
+let c = 0;
 function loop() {
   let time = window.requestAnimationFrame(loop);
-
   if (!ctx || !canvas) return;
+  // console.log("total:", lines);
   ctx.globalCompositeOperation = "source-over";
+  ctx.save();
+  // ctx.beginPath();
   ctx.shadowBlur = 0;
-
   ctx.fillStyle = "rgba(0,0,0,alp)".replace("alp", "0.04");
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.globalCompositeOperation = "lighter";
+  // ctx.closePath();
+  ctx.restore();
 
   let dy = 2;
-
+  lines = lines.filter((l) => l.died !== true);
   for (const line of lines) {
     if (!line.died)
-      if (ctx && line.start) {
-        if (line.type === "row") {
-          line.y = line.initialY - Math.sin(counter) * 15;
-          line.x += dy;
-        } else {
-          line.x = line.initialX - Math.sin(counter) * 15;
-          line.y += dy;
-        }
-        counter += increase;
-      } else {
+      if (ctx) {
+        // if (c++ < 1) {
+
+        // }
+        // if (line.type === "row") {
+        //   line.y = line.initialY + Math.sin(counter) * 15;
+        //   line.x += dy;
+        // } else {
+        //   // line.x = Math.sin(counter) * 15;
+        //   // line.y += dy;
+        // }
+        // counter += increase;
         if (line.type == "row") {
           line.x += 2;
         } else {
           line.y += 2;
         }
       }
+    // } else {
+    // }
     die(line);
-    if (line.died) {
-      lines = lines.filter((l) => l.id !== line.id);
-    }
 
     ctx.beginPath();
     ctx.fillRect(line.x, line.y, 1, 1);
     ctx.shadowColor = "hsl(100,100%,50%)";
     ctx.shadowBlur = 1;
     ctx.fillStyle = "hsl(hue,100%,50%)".replace("hue", time.toString());
-
     ctx.closePath();
   }
 }
