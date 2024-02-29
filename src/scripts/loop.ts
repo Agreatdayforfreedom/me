db22ed206f36472307439ec19bca83d33e373d54
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-02-28 23:03:04 -0300

    options and shadows

--- src/scripts/loop.ts
+++ src/scripts/loop.ts
@@ -5,10 +5,14 @@ let canvas = document.querySelector("canvas");
 let ctx = canvas?.getContext("2d");
 
 let lines: Line[] = [];
-let test: number[] = [];
 const padding = 50;
 const margin = 20;
-const probability = 1;
+const probability = 0.1;
+
+const options = {
+  shadowMultiplier: 2.5,
+};
+
 function drawCanvas() {
   if (!canvas) return;
 
@@ -53,58 +57,53 @@ function drawCanvas() {
   }
 }
 drawCanvas();
-var increase = ((90 / 180) * Math.PI) / 9;
-let speed = Math.random() * 20;
-let counter = 0;
-counter += increase;
-let start = 300;
-let c = 0;
+//TODO
+// var increase = ((90 / 180) * Math.PI) / 9;
+// let speed = Math.random() * 20;
+// let counter = 0;
+// let start = 300;
+// let c = 0;
 function loop() {
   let time = window.requestAnimationFrame(loop);
   if (!ctx || !canvas) return;
-  // console.log("total:", lines);
+
   ctx.globalCompositeOperation = "source-over";
   ctx.save();
-  // ctx.beginPath();
   ctx.shadowBlur = 0;
   ctx.fillStyle = "rgba(0,0,0,alp)".replace("alp", "0.04");
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.globalCompositeOperation = "lighter";
-  // ctx.closePath();
   ctx.restore();
 
-  let dy = 2;
   lines = lines.filter((l) => l.died !== true);
   for (const line of lines) {
-    if (!line.died)
-      if (ctx) {
-        // if (c++ < 1) {
+    ctx.beginPath();
+    ctx.fillRect(line.x, line.y, 2, 2);
+    ctx.fillStyle = ctx.shadowColor = "hsl(hue,100%,50%)".replace("hue", time.toString());
 
-        // }
+    ctx.globalCompositeOperation = "lighter";
+
+    ctx.shadowBlur = Math.random() * options.shadowMultiplier;
+    ctx.closePath();
+
+    if (!line.died)
+      if (line.type == "row") {
         // if (line.type === "row") {
-        //   line.y = line.initialY + Math.sin(counter) * 15;
-        //   line.x += dy;
+        //   // if (line.type == "row") {
+        //   // if (ctx) {
+        //   line.y = line.initialY + Math.sin(counter) * 9;
+        //   line.x += 5;
         // } else {
-        //   // line.x = Math.sin(counter) * 15;
-        //   // line.y += dy;
+        //   line.x = line.initialX + Math.sin(counter) * 9;
+        //   line.y += 5;
         // }
         // counter += increase;
-        if (line.type == "row") {
-          line.x += 2;
-        } else {
-          line.y += 2;
-        }
+        line.x += 2;
+      } else {
+        line.y += 2;
       }
-    // } else {
-    // }
-    die(line);
 
-    ctx.beginPath();
-    ctx.fillRect(line.x, line.y, 1, 1);
-    ctx.shadowColor = "hsl(100,100%,50%)";
-    ctx.shadowBlur = 1;
-    ctx.fillStyle = "hsl(hue,100%,50%)".replace("hue", time.toString());
-    ctx.closePath();
+    die(line);
   }
 }
 
